7e36271f9ba6cac1f99721701db19f151439c6f4
Roberto Jerez <jerezr@sheridancollege.ca>
2020-04-11 22:12:18 -0400

    Regsiter page

--- RestarauntReservation/Controllers/HomeController.cs
+++ RestarauntReservation/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace RestarauntReservation.Controllers {
             return View("Login");
         }
 
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ RestarauntReservation/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace RestarauntReservation.Models
+{
+    public class User
+    {
+        public String Name { get; set; }
+        public String UserName { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+    }
+}
